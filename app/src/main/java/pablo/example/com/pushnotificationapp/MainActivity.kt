5bc27bc76ae6c52d68063d67fac6f78e5c84a151
Pablo Guardado Alvarez <pablo.guardado@live.u-tad.com>
2019-01-14 12:46:06 +0100

    Todo codigo, faltan comentarios

---
+++ app/src/main/java/pablo/example/com/pushnotificationapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package pablo.example.com.pushnotificationapp
+
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationUtils(this)
+        }
+    }
+}
