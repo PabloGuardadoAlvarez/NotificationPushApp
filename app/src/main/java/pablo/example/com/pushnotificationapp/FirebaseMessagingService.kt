957786b891e1149dcc797d82808d700dc0678f05
Pablo Guardado Alvarez <pablo.guardado@live.u-tad.com>
2019-01-14 12:50:31 +0100

    comentarios creados

--- app/src/main/java/pablo/example/com/pushnotificationapp/FirebaseMessagingService.kt
+++ app/src/main/java/pablo/example/com/pushnotificationapp/FirebaseMessagingService.kt
@@ -20,9 +20,11 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     @SuppressLint("LongLogTag")
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "Dikirim dari: ${remoteMessage.from}")
+        //llamamos a la funcion showNotification
         showNotification(remoteMessage)
     }
 
+    // creamos una funcion en la que creamos el id de la notificacion y el notification manager.
     private fun showNotification(remoteMessage: RemoteMessage){
 
         val mNotificationID = 101
@@ -31,25 +33,27 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     }
 
-
+// funcion en la que creamos la notificacion
     private fun defaultNotification(remoteMessage: RemoteMessage) = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         Notification.Builder(this, NotificationUtils.CHANNEL_ID)
     }
     else {
         Notification.Builder(this)
     }.apply {
+        // creamos las notificacion y seteamos el titulo y el body con los datos del remoteMessage
         setContentTitle(remoteMessage.notification?.title)
         setContentText(remoteMessage.notification?.body)
         setAutoCancel(false)
         setSmallIcon(android.R.drawable.ic_dialog_info)
     }
 
-
+//creamos el intent al MainActivity
     private fun notificationIntent(remoteMessage: RemoteMessage) = PendingIntent.getActivity(this,
             0,
             Intent(this, MainActivity::class.java),
             //creamos un pending intent, que es un intent que se ejecutara mas tarde
             PendingIntent.FLAG_UPDATE_CURRENT).run {
+    //llamamos a la funcion con la que creamos la notificacion y le pasamos el remoteMessage
         defaultNotification(remoteMessage).setContentIntent(this).build()
     }
 
